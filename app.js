a281cd6b98c7d9d9328c9951fd4d96afedd7a496
Luis Carter <LuisCarter11078@gmail.com>
2024-07-17 16:31:42 -0500

    Navigation complete however, there are issues

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+// NAV BUTTONS
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+    container.classList.add("change");
+});
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+    container.classList.remove("change");
+});
+
+
+// DYNAMIC COLORS FOR NAVBAR
+const colors = ["#6495ed", "#77ffd4", "#ffa07a", "#f08080", "#afeeee"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).
+forEach(item => {
+    item.style.cssText = `background-color: ${colors[i++]
+    }`;
+});
+
+
+// CARD SWITCHING FOR STORIES SECTION
 Array.from(document.querySelectorAll
 (".navigation-button")).forEach((item) => {
     item.onclick = () => {
